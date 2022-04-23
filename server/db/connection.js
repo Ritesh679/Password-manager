acb2745169cbfa1acb4ab179dd12f4b85ee6a22f
Ritesh679 <poudel123.rp@gmail.com>
2022-04-23 14:38:46 +0530

    added files

---
+++ server/db/connection.js
@@ -0,0 +1,19 @@
+// import mongoose from "mongoose";
+const mongoose = require('mongoose'); 
+const DB = process.env.DATABASE;
+
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+.then(() =>
+{
+    console.log("Connection success.");
+})
+.catch((error) =>
+{
+    console.log(error)
+})
